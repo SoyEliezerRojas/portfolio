695c98e2be77f133320cc313ddff445f61e5de89
Eliezer Rojas <e.rojas@ibm.com>
2024-07-08 21:58:36 -0300

    header terminado con responsive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-let container = document.getElementById("Container")
+const open = document.querySelector("#open_menu")
+const close = document.querySelector("#close_menu")
+const nav = document.querySelector("#header_nav")
 
-container.append(" Ready")
+console.log("Hola")
+
+open.addEventListener("click",()=>{
+    nav.classList.add("show")
+    nav.classList.remove("hide")
+})
+
+close.addEventListener("click",()=>{
+    nav.classList.add("hide")
+    nav.classList.remove("show")
+})
